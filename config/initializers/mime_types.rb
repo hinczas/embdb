5b1ee26bb0bfff94a0f25a85c36f54a8db9e577e
rafal <hinczas@gmail.com>
2018-02-01 11:10:59 +0000

    mime types updated

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,5 +2,16 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "*/*", :all
+Mime::Type.register "text/plain", :text, [], %w(txt)
+Mime::Type.register "text/html", :html, %w( application/xhtml+xml ), %w( xhtml )
+Mime::Type.register "text/javascript", :js, %w( application/javascript application/x-javascript )
+Mime::Type.register "text/css", :css
+Mime::Type.register "text/calendar", :ics
 Mime::Type.register "text/csv", :csv
+Mime::Type.register "application/xml", :xml, %w( text/xml application/x-xml )
+Mime::Type.register "application/rss+xml", :rss
+Mime::Type.register "application/atom+xml", :atom
+Mime::Type.register "application/x-yaml", :yaml, %w( text/yaml )
+Mime::Type.register "text/tab-separated-values", :tsv unless defined? Mime::TSV
 
