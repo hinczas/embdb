8ce224e81002222ad7a4d9b216bb1a77c3229646
rafal <hinczas@gmail.com>
2018-02-02 19:34:10 +0000

    removed obsolete migrations

--- db/migrate/20160520081344_add_foreign_key.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKey < ActiveRecord::Migration
-  def change
-	add_foreign_key :parts, :manufacturers
-	add_foreign_key :parts, :locations
-	add_foreign_key :parts, :packages
-  end
-end
